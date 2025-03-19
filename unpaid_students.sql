8a683e1da64d253181c63ffc4f2381acd1414203
devToms <tomaszkedziora14@example.com>
2025-03-19 12:37:41 +0100

    added comments

--- unpaid_students.sql
+++ unpaid_students.sql
@@ -7,14 +7,17 @@ WITH unpaid_students AS (
         students s
     LEFT JOIN 
         payments p ON s.id = p.student_id 
-        AND p.payment_date <= '2025-03-31' 
+        AND p.payment_date <= '2025-03-31' -- Last day of the first quarter
     WHERE 
-        p.id IS NULL 
+        p.id IS NULL -- Students who have not made payment
 )
 INSERT INTO fines (student_id, fine_amount, fine_date)
 SELECT 
     us.student_id,
-    100 AS fine_amount,  
-    CURRENT_DATE AS fine_date  
+    100 AS fine_amount,   -- -- Penalty amount
+    CURRENT_DATE AS fine_date  -- Date of imposition of penalty
 FROM 
-    unpaid_students us;
+    unpaid_students us;  
+
+
+-- ???
